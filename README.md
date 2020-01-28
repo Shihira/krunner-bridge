bcffce4d8752d1d2b975022696d95252b2a8ca85
alex1701c <alexkp12355@gmail.com>
2020-01-28 08:36:05 +0100

    Create python API for new signals, add template

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # KRunner Bridge
 
-### NOTE: This branch is currently developed and supports only python3 as a plugin language,
-other languages and new features will come soon !
-
-
 **Write krunner python plugins the quick way.**
 
 <del>Plasma 5 stopped exposing KDE Framework API to script languages (except QML) for the purpose of so-called stability (but it is not at all stable, crashing from time to time). But this brings some horrible consequence that writing krunner plugins becomes time-consuming. You have to write long C++ code, configure your C++ building toolchain, and compile them each time you modify something.</del>
--- python/krunner_bridge/__init__.py
+++ python/krunner_bridge/__init__.py
@@ -82,16 +82,28 @@ class datasource(object):
         return "<krunner_bridge.datasource text={0} cmp={1}>".format(
             repr(self.text), repr(self.cmp))
 
-__MATCH_HANDLER = lambda query: []
-def match_handler(func):
-    global __MATCH_HANDLER
-    __MATCH_HANDLER = func
-    return func
-
 __INIT_HANDLER = lambda: None
 def init_handler(func):
     global __INIT_HANDLER
     __INIT_HANDLER = func
+    return func
+
+__SETUP_HANDLER = lambda: None
+def setup_handler(func):
+    global __SETUP_HANDLER
+    __SETUP_HANDLER = func
+    return func
+
+__TEARDOWN_HANDLER = lambda: None
+def teardown_handler(func):
+    global __TEARDOWN_HANDLER
+    __TEARDOWN_HANDLER = func
+    return func
+
+__MATCH_HANDLER = lambda query: []
+def match_handler(func):
+    global __MATCH_HANDLER
+    __MATCH_HANDLER = func
     return func
 
 __RUN_HANDLER = lambda data: None
@@ -125,7 +137,16 @@ def exec():
         print(json.dumps({ "result": result }))
 
     if op == "init":
-        __INIT_HANDLER(**args)
+        __INIT_HANDLER()
+
+    if op == "setup":
+        __SETUP_HANDLER()
+
+    if op == "match":
+        __MATCH_HANDLER(**args)
+
+    if op == "teardown":
+        __TEARDOWN_HANDLER()
 
     if op == "run":
         __RUN_HANDLER(**args)
